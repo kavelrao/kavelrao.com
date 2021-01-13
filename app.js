0a0eb80d0aa1fc9aad1c84fa3315c513ce8284ea
kavelrao <rao.kavel@gmail.com>
2021-01-13 01:25:55 -0800

    re-enable helmet, resume name change

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-/*
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
@@ -23,7 +22,6 @@ app.use(helmet({
     },
   }
 }));
-*/
 
 app.use(compression());
 app.use(logger('dev'));
