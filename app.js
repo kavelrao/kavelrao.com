09248f0b08b4e3932286d3febd06815b891cae49
kavelrao <rao.kavel@gmail.com>
2021-01-13 01:34:59 -0800

    helmet test

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
@@ -22,13 +29,6 @@ app.use(helmet({
     },
   }
 }));
-
-app.use(compression());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
